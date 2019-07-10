a36cd7d6ac4f8bcea4fb5ce3260e463fcc14374e
dave <david.f.cotton@gmail.com>
2019-07-10 14:32:28 +1000

    Swap out old numpy game for improved version

--- gym_connect4/envs/connect4_env.py
+++ gym_connect4/envs/connect4_env.py
@@ -1,5 +1,5 @@
 import copy
-from typing import Set, Tuple
+from typing import List, Optional, Set, Tuple
 
 import gym
 from gym import spaces
@@ -8,159 +8,175 @@ import numpy as np
 
 BOARD_HEIGHT = 6
 BOARD_WIDTH = 7
+WIN_LENGTH = 4
 REWARD_WIN = 1.0
 REWARD_LOSS = -1.0
+REWARD_DRAW = 0
 
 
 class Connect4Env(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, initial_state=None, initial_player=None) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.action_space = spaces.Discrete(BOARD_WIDTH)
         self.observation_space = spaces.Box(low=0, high=2, shape=(BOARD_HEIGHT, BOARD_WIDTH), dtype=np.uint8)
-        self.board = np.zeros([BOARD_HEIGHT, BOARD_WIDTH], dtype=np.uint8)
-        self.player = 1
-        self.game_over = False
-        self.winner = None
-        self.reset(initial_state, initial_player)
+        self.game = Connect4()
 
-    def reset(self, state=None, player=None) -> np.ndarray:
-        if state is not None and player is not None:
-            self.board = state.copy().reshape((BOARD_HEIGHT, BOARD_WIDTH))
-            self.player = player
-            self.time = np.count_nonzero(state)
+    def reset(self) -> np.ndarray:
+        self.game = Connect4()
+        return self.get_state()
 
-            # all_arr = []
-            # for column in range(self.board.shape[1]):
-            #     column_vec = self.board[:, column]
-            #     non_zero = np.where(column_vec != 0)[0]
-            #     i = non_zero[0] - 1 if len(non_zero) else self.board.shape[0] - 1
-            #
-            #     all_arr.extend(self._get_axes(self.board, i, column))
-            #     all_arr.extend(self._get_diagonals(self.board, i, column))
-            #
-            # for arr in all_arr:
-            #     winner = self._winning_rule(arr)
-            #     if winner:
-            #         self.game_over = True
-            #         self.winner = self.player
-
-        return self._get_state()
-
-    def step(self, action) -> Tuple[np.ndarray, float, bool, dict]:
+    def step(self, column) -> Tuple[np.ndarray, float, bool, dict]:
         """Make a game action.
 
         Throws a ValueError if trying to drop into a full column.
 
-        :param action: The column index to drop in.
+        :param column: The column index to drop in ([0-7]).
         :return: A tuple containing the next state, reward, if the game ended and an empty info dict.
         """
-        if not self.game_over:
-            remaining_rows = self._drop(self.player, action)
-            if remaining_rows < 0:
-                raise ValueError('Invalid action, column full.')
-            self._winning_check(remaining_rows, action)
+        if not self.game.is_valid_move(column):
+            raise ValueError('Invalid action, column %s is full' % column)
+        self.game.move(column)
+        state = self.get_state()
+        reward = self.game.get_reward()
+        game_over = self.game.is_game_over()
 
-        reward = 0.0
-        if self.game_over:
-            reward = REWARD_WIN if self.player == self.winner else REWARD_LOSS
-
-        if np.all(self.board):  # check if board full
-            self.game_over = True
-            self.winner = 0  # there was no winner
-
-        info = {}
-
-        self.player = 1 if self.player == 2 else 2
-        self.time += 1
-
-        return self._get_state(), reward, self.game_over, info
-
-    def _drop(self, player, column) -> int:
-        """Drop a coin into the specified column.
-
-        :param player: The player making the action.
-        :param column: The column the coin is being placed.
-        :return: The number of remaining rows above the placed coin.
-        """
-        column_vec = self.board[:, column]
-        non_zero = np.where(column_vec != 0)[0]
-
-        if non_zero.size == 0:
-            # sets the stone to the last element
-            i = self.board.shape[0] - 1
-            self.board[i, column] = player
-        else:
-            # sets the stone on the last 0
-            i = non_zero[0] - 1
-            if i >= 0:
-                self.board[i, column] = player
-        return i
+        return state, reward, game_over, {}
 
     def valid_actions(self) -> Set[int]:
         """Fetch a set of valid moves available.
 
         :return: A set of valid moves.
         """
-        valid_moves = set()
-        for column in range(self.board.shape[1]):
-            column_vec = self.board[:, column]
-            nonzero = np.count_nonzero(column_vec)
-            if nonzero < self.board.shape[0]:
-                valid_moves.add(column)
+        valid_moves = set(self.game.get_moves())
         return valid_moves
 
-    def _winning_check(self, i, action) -> None:
-        """Checks if there is four equal numbers in every row, column and diagonal of the matrix.
-
-        :param i:
-        :param action: The column the player dropped into.
-        """
-        all_arr = []
-        all_arr.extend(self._get_axes(self.board, i, action))
-        all_arr.extend(self._get_diagonals(self.board, i, action))
-
-        for arr in all_arr:
-            winner = self._winning_rule(arr)
-            if winner:
-                self.game_over = True
-                self.winner = self.player
-
-    def _winning_rule(self, arr) -> bool:
-        win1rule = np.array([1, 1, 1, 1])
-        win2rule = np.array([2, 2, 2, 2])
-        # subarrays of len = 4
-        sub_arrays = [arr[i:i + 4] for i in range(len(arr) - 3)]
-
-        player1wins = any([np.array_equal(win1rule, sub) for sub in sub_arrays])
-        player2wins = any([np.array_equal(win2rule, sub) for sub in sub_arrays])
-
-        if player1wins or player2wins:
-            return True
-        else:
-            return False
-
-    def _get_diagonals(self, board, i, j) -> list:
-        diags = []
-        diags.append(np.diagonal(board, offset=(j - i)))
-        diags.append(np.diagonal(np.rot90(board), offset=-board.shape[1] + (j + i) + 1))
-        return diags
-
-    def _get_axes(self, board, i, j) -> list:
-        return [board[i, :], board[:, j]]
-
-    def _get_state(self) -> np.ndarray:
-        return self.board.copy()
+    def get_state(self) -> np.ndarray:
+        board = self.game.board.copy()
+        state = np.flip(board, axis=0)
+        return state
 
     def render(self, mode='human') -> None:
         print('  1 2 3 4 5 6 7')
         print(' ---------------')
-        print(self._get_state())
+        print(self.get_state())
         print(' ---------------')
         print('  1 2 3 4 5 6 7')
-        print()
-        if self.winner is not None:
-            print('##########')
-            print('Winner:', self.winner)
-            print('##########')
+        # print()
+        # if self.game.is_game_over():
+        #     print('##########')
+        #     print('Winner:', self.game.player + 1)
+        #     print('##########')
+
+    def winner(self) -> int:
+        if not self.game.is_game_over():
+            return -1
+        return 0 if self.game.is_winner(0) else 1
+
+    def time(self):
+        return np.count_nonzero(self.game.board)
+
+
+class Connect4:
+    def __init__(self) -> None:
+        super().__init__()
+        self.board = np.zeros((BOARD_HEIGHT, BOARD_WIDTH), dtype=np.uint8)
+        self.bit_board = [0, 0]  # bit-board for each player
+        self.dirs = [1, (BOARD_HEIGHT + 1), (BOARD_HEIGHT + 1) - 1, (BOARD_HEIGHT + 1) + 1]  # this is used for bitwise operations
+        self.heights = [(BOARD_HEIGHT + 1) * i for i in range(BOARD_WIDTH)]  # top empty row for each column
+        self.lowest_row = [0] * BOARD_WIDTH  # number of stones in each row
+        self.top_row = [(x * (BOARD_HEIGHT + 1)) - 1 for x in range(1, BOARD_WIDTH + 1)]  # top row of the board (this will never change)
+        self.player = 1
+
+    def clone(self):
+        clone = Connect4()
+        clone.bit_board = copy.deepcopy(self.bit_board)
+        clone.heights = copy.deepcopy(self.heights)
+        clone.lowest_row = copy.deepcopy(self.lowest_row)
+        clone.board = copy.deepcopy(self.board)
+        clone.top_row = copy.deepcopy(self.top_row)
+        clone.player = self.player
+        return clone
+
+    def move(self, column) -> None:
+        m2 = 1 << self.heights[column]  # position entry on bit-board
+        self.heights[column] += 1  # update top empty row for column
+        self.player ^= 1
+        self.bit_board[self.player] ^= m2  # XOR operation to insert stone in player's bit-board
+        self.board[self.lowest_row[column]][column] = self.player + 1  # update entry in matrix (only for printing)
+        self.lowest_row[column] += 1  # update number of stones in column
+
+    def get_reward(self, player: int = None) -> Optional[float]:
+        if player is None:
+            player = self.player
+
+        if self.is_winner(player):
+            return REWARD_WIN  # player wins
+        elif self.is_winner(player ^ 1):
+            return REWARD_LOSS  # if opponent wins
+        elif self.is_draw():
+            return REWARD_DRAW
+        else:
+            return 0.0
+
+    def is_winner(self, player: int = None) -> bool:
+        """Evaluate board, find out if a player has won.
+
+        :param player: The player to check.
+        :return: True if the player has won, otherwise False.
+        """
+        if player is None:
+            player = self.player
+
+        for d in self.dirs:
+            bb = self.bit_board[player]
+            for i in range(1, WIN_LENGTH):
+                bb &= self.bit_board[player] >> (i * d)
+            if bb != 0:
+                return True
+        return False
+
+    def is_draw(self) -> bool:
+        """Is the game a draw?
+
+        :return: True if the game is drawn, else False.
+        """
+        return not self.get_moves() and not self.is_winner(self.player) and not self.is_winner(self.player ^ 1)
+
+    def is_game_over(self) -> bool:
+        """Is the game over?
+
+        :return: True if the game is over, else False.
+        """
+        return self.is_winner(self.player) or self.is_winner(self.player ^ 1) or not self.get_moves()
+
+    # returns list of available moves
+    def get_moves(self) -> List[int]:
+        if self.is_winner(self.player) or self.is_winner(self.player ^ 1):
+            return []  # if terminal state, return empty list
+
+        list_moves = []
+        for i in range(BOARD_WIDTH):
+            if self.lowest_row[i] < BOARD_HEIGHT:
+                list_moves.append(i)
+        return list_moves
+
+    # def valid_actions(self) -> Set[int]:
+    #     """Fetch a set of valid moves available.
+    #
+    #     :return: A set of valid moves.
+    #     """
+    #     valid_moves = set()
+    #     for column in range(self.board.shape[1]):
+    #         if self.is_valid_move(column):
+    #             valid_moves.add(column)
+    #     return valid_moves
+
+    def is_valid_move(self, column: int) -> bool:
+        """Check if column is full.
+
+        :param column: The column to check
+        :return: True if it is a valid move, else False.
+        """
+        return self.heights[column] != self.top_row[column]
